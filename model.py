7a2dcb0c1cb75de7b152ddaad4eff0175c227399
Richard Alexander <alexanderrichard@gmail.com>
2018-04-04 15:09:27 -0500

    trained cropped and full images with no ROIs

--- model.py
+++ model.py
@@ -2283,7 +2283,7 @@ class MaskRCNN():
             validation_steps=self.config.VALIDATION_STEPS,
             max_queue_size=100,
             workers=workers,
-            use_multiprocessing=True,
+            use_multiprocessing=False, #RLA EDIT (was true)<---------------------------------------------------------------------
         )
         self.epoch = max(self.epoch, epochs)
 
